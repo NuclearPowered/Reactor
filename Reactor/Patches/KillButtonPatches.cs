28c9d30c3ee312ffd0443954d6fcc816ec49093d
js6pak <kubastaron@hotmail.com>
2021-02-06 17:25:15 +0100

    Keep correct killTimer when KillCooldown is 0

--- Reactor/Patches/KillButtonPatches.cs
+++ Reactor/Patches/KillButtonPatches.cs
@@ -2,30 +2,28 @@
 
 namespace Reactor.Patches
 {
+    /// <summary>
+    /// Fixes kill cooldown resetting for a few frames if anyone on the map got the kill
+    /// Also fixes invisible kill button if KillCooldown is set to 0
+    /// </summary>
     internal class KillButtonPatches
     {
-        // Fixes kill cooldown resetting for a few frames if anyone on the map got the kill
-        // Also fixes disappearing kill button if KillCooldown set to 0
         [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.SetKillTimer))]
         [HarmonyPriority(Priority.First)]
-        public static class PlayerControlSetKillTimerPatch
+        public static class SetKillTimerPatch
         {
             public static bool Prefix(PlayerControl __instance, [HarmonyArgument(0)] float time)
             {
                 __instance.killTimer = time;
-                
+
                 // Patched because of this - game doesn't check if you're the killer
                 if (__instance.AmOwner)
                 {
-                    // SetCooldown does timer/maxTimer, so if MaxTimer is 0 kill button disappears
-                    if (PlayerControl.GameOptions.KillCooldown == 0)
-                    {
-                        HudManager.Instance.KillButton.SetCoolDown(0, 1);
-                    }
-                    else
-                    {
-                        HudManager.Instance.KillButton.SetCoolDown(time, PlayerControl.GameOptions.KillCooldown);
-                    }
+                    HudManager.Instance.KillButton.SetCoolDown(
+                        time,
+                        // if MaxTimer is 0, kill button disappears
+                        PlayerControl.GameOptions.KillCooldown <= 0 ? 1 : PlayerControl.GameOptions.KillCooldown
+                    );
                 }
 
                 return false;
