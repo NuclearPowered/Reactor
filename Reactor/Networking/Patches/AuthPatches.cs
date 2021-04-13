ae213c6b37683494f84b3af736767ef8c0d1d59b
js6pak <kubastaron@hotmail.com>
2021-04-13 17:05:35 +0200

    Fix auth patch

--- Reactor/Networking/Patches/AuthPatches.cs
+++ Reactor/Networking/Patches/AuthPatches.cs
@@ -6,11 +6,11 @@ namespace Reactor.Networking.Patches
     internal static class AuthPatches
     {
         [HarmonyPatch(typeof(X509Certificate2Collection), nameof(X509Certificate2Collection.Contains))]
-        public static class AcceptCustomerCertificatesPatch
+        public static class AcceptCustomCertificatesPatch
         {
             public static bool Prefix(X509Certificate2Collection __instance, ref bool __result)
             {
-                if (AuthManager.Instance && AuthManager.Instance.connection != null && AuthManager.Instance.connection.serverCertificates == __instance)
+                if (AuthManager.Instance && AuthManager.Instance.connection != null && AuthManager.Instance.connection.serverCertificates.Equals(__instance))
                 {
                     __result = true;
                     return false;
