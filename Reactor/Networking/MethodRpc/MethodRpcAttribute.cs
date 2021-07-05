82a8e9b31fb4dc99a2105ea204b85c40b15fecf1
Zirno <ziadmontaser2005@gmail.com>
2021-07-05 21:44:01 +0200

    method Rpc

---
+++ Reactor/Networking/MethodRpc/MethodRpcAttribute.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+using Hazel;
+
+namespace Reactor.Networking.MethodRpc
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class MethodRpcAttribute : Attribute
+    {
+        public byte id;
+        public SendOption option;
+        public RpcLocalHandling localHandling;
+
+        public MethodRpcAttribute( byte id, SendOption option = SendOption.Reliable, RpcLocalHandling localHandling = RpcLocalHandling.Before)
+        {
+            this.id = id;
+            this.option = option;
+            this.localHandling = localHandling;
+        }
+        
+        public static void Register(Assembly assembly, BasePlugin plugin)
+        {
+            Logger<ReactorPlugin>.Info($"Registering Method Rpc for {assembly.GetName()}");
+            Dictionary<int, MethodInfo> serializeMethods = new();
+
+            MethodInfo serializeMethod;
+            int index = 0;
+
+            while ((serializeMethod = RpcSerializationHandels.GetHandleFor(index)) != null)
+            {
+                serializeMethods.Add(index, serializeMethod);
+                Logger<ReactorPlugin>.Debug($"Registered Rpc Serialization Handle for {index} args");
+                index++;
+            }
+            
+            var rpcMethods = assembly.GetTypes()
+                .SelectMany(t => t.GetMethods(BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public))
+                .Where(m => m.GetCustomAttributes(typeof(MethodRpcAttribute), false).Length > 0)
+                .ToArray();
+            
+            
+            foreach (var method in rpcMethods)
+            {
+                if (!method.IsStatic)
+                {
+                    Logger<ReactorPlugin>.Warning("Cannot register non static custom method rpc");
+                    continue;
+                }
+
+                var seializer = serializeMethods[method.GetParameters().Length];
+                if (seializer == null)
+                {
+                    Logger<ReactorPlugin>.Error($"Unsupported Args count, max is {RpcSerializationHandels.GetMaxArgs()}");
+                    continue;
+                }
+
+                var attribute = method.GetCustomAttribute<MethodRpcAttribute>();
+                if (attribute.localHandling == RpcLocalHandling.Before)
+                {
+                    PluginSingleton<ReactorPlugin>.Instance.Harmony.Patch(method, new HarmonyMethod(seializer));
+                }
+                else if (attribute.localHandling == RpcLocalHandling.After)
+                {
+                    PluginSingleton<ReactorPlugin>.Instance.Harmony.Patch(method, postfix: new HarmonyMethod(seializer));
+                }
+                var customRpc = new CustomMethodRpc(plugin, method, attribute.id, attribute.option, attribute.localHandling);
+                try
+                {
+                    PluginSingleton<ReactorPlugin>.Instance.CustomRpcManager.Register(customRpc);
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            }
+        }
+        
+        internal static void Initialize()
+        {
+            ChainloaderHooks.PluginLoad += plugin => Register(plugin.GetType().Assembly, plugin);
+        }
+
+        [MethodRpc(123)]
+        static void Hello()
+        {
+            
+        }
+    }
+}
---
+++ Reactor/Networking/MethodRpc/RpcSerializer.cs
@@ -0,0 +1,179 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Hazel;
+using InnerNet;
+using Reactor;
+using Reactor.Extensions;
+using Reactor.Networking.MethodRpc;
+using UnityEngine;
+
+namespace Reactor.Networking
+{
+    public static class RpcSerializer
+    {
+        public static void SendMassage(MessageWriter writer, params object[] list)
+        {
+            Logger<ReactorPlugin>.Debug($"Serializing {list.Length} object");
+
+            foreach (var o in list)
+            {
+                if (o.GetType().IsValueType  && !o.GetType().IsPrimitive)
+                {
+                    var structFeilds = o.GetType().GetFields(BindingFlags.Public | BindingFlags.Instance);
+                    foreach (var feild in structFeilds)
+                    {
+                        writer.Write(feild.GetValue(o));
+                    }
+                }
+                else
+                {
+                    writer.Write(o);
+                }
+            }
+        }
+
+        public static void Write(this MessageWriter writer, object o)
+        {
+            switch (o)
+            {
+                case int i:
+                    writer.WritePacked(i);
+                    Logger<ReactorPlugin>.Debug($"Serialized int");
+                    break;
+                case uint i:
+                    writer.WritePacked(i);
+                    Logger<ReactorPlugin>.Debug($"Serialized uint");
+                    break;
+                case byte i:
+                    writer.Write(i);
+                    Logger<ReactorPlugin>.Debug($"Serialized byte");
+                    break;
+                case float i:
+                    writer.Write(i);
+                    Logger<ReactorPlugin>.Debug($"Serialized float");
+                    break;
+                case sbyte i:
+                    writer.Write(i);
+                    Logger<ReactorPlugin>.Debug($"Serialized sbyte");
+                    break;
+                case ushort i:
+                    writer.Write(i);
+                    Logger<ReactorPlugin>.Debug($"Serialized ushort");
+                    break;
+                case bool i:
+                    writer.Write(i);
+                    Logger<ReactorPlugin>.Debug($"Serialized bool");
+                    break;
+                case Vector2 i:
+                    writer.Write(i);
+                    Logger<ReactorPlugin>.Debug($"Serialized Vector2");
+                    break;
+                case string i:
+                    writer.Write(i);
+                    Logger<ReactorPlugin>.Debug($"Serialized string");
+                    break;
+                default:
+                    if (o.GetType().IsSubclassOf(typeof(InnerNetObject)))
+                    {
+                        Logger<ReactorPlugin>.Debug($"Serialized {nameof(InnerNetObject)}");
+                        writer.WritePacked(((InnerNetObject)o).NetId);
+                    }
+                    else
+                    {
+                        Logger<ReactorPlugin>.Warning($"Tried serializing unsupported type {nameof(o)}");
+                    }
+
+                    break;
+            }
+        }
+
+
+        public static object[] Deserialize(MessageReader reader, Type[] list)
+        {
+            Logger<ReactorPlugin>.Debug($"Deserializing {list.Length} object");
+
+            List<object> args = new();
+
+            foreach (var t in list)
+            {
+                if (t.IsValueType && !t.IsPrimitive)
+                {
+                    List<object> structArgs = new();
+                    var feilds = t.GetFields(BindingFlags.Public | BindingFlags.Instance);
+                    foreach (var feild in feilds)
+                    {
+                        structArgs.Add(reader.Read(feild.GetType()));
+                    }
+
+                    args.Add(Activator.CreateInstance(t, structArgs));
+                }
+                else
+                {
+                    args.Add(reader.Read(t));
+                }
+            }
+
+            return args.ToArray();
+        }
+
+
+        public static object Read(this MessageReader reader, Type t)
+        {
+            if (t == typeof(int) )
+            {
+                return reader.ReadPackedInt32();
+            }
+
+            if (t == typeof(uint))
+            {
+                return (reader.ReadPackedUInt32());
+            }
+
+            if (t == typeof(byte))
+            {
+                return (reader.ReadByte());
+            }
+
+            if (t == typeof(float))
+            {
+                return (reader.ReadSingle());
+            }
+
+            if (t == typeof(sbyte))
+            {
+                return (reader.ReadSByte());
+            }
+
+            if (t == typeof(ushort))
+            {
+                return (reader.ReadUInt16());
+            }
+
+            if (t == typeof(bool))
+            {
+                return (reader.ReadBoolean());
+            }
+
+            if (t == typeof(Vector2))
+            {
+                return (reader.ReadVector2());
+            }
+
+            if (t == typeof(string))
+            {
+                return (reader.ReadString());
+            }
+
+            if (t.IsSubclassOf(typeof(InnerNetObject)))
+            {
+                return typeof(AmongUsClient).GetMethod(nameof(AmongUsClient.FindObjectByNetId)).MakeGenericMethod(t)
+                    .Invoke(AmongUsClient.Instance, new object[]{reader.ReadPackedUInt32()});
+            }
+
+            Logger<ReactorPlugin>.Warning($"Tried deserializing unsupported type {t.BaseType.Name}");
+            return null;
+
+        }
+    }
+}
