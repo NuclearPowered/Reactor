e58f8b5aef9091427ae3e6f5e7499bdc038c2605
js6pak <kubastaron@hotmail.com>
2022-02-27 15:52:04 +0100

    Update CustomServersPatch for 2022.2.8+

--- Reactor/Patches/CustomServersPatch.cs
+++ Reactor/Patches/CustomServersPatch.cs
@@ -1,52 +1,17 @@
 using HarmonyLib;
-using Hazel.Udp;
-using Il2CppSystem.Net;
-using InnerNet;
 
 namespace Reactor.Patches
 {
+    [HarmonyPatch]
     internal static class CustomServersPatch
     {
-        /// <summary>
-        /// Send the account id only to Among Us official servers
-        /// </summary>
-        [HarmonyPatch(typeof(InnerNetClient), nameof(InnerNetClient.GetConnectionData))]
-        public static class DontSendAccountIdPatch
+        [HarmonyPatch(typeof(AuthManager._CoConnect_d__4), nameof(AuthManager._CoConnect_d__4.MoveNext))]
+        [HarmonyPatch(typeof(AuthManager._CoWaitForNonce_d__6), nameof(AuthManager._CoWaitForNonce_d__6.MoveNext))]
+        [HarmonyPrefix]
+        public static bool DisableAuthServerPatch(out bool __result)
         {
-            public static void Prefix(ref bool useDtlsLayout)
-            {
-                if (AmongUsClient.Instance.GameMode != GameModes.OnlineGame)
-                {
-                    useDtlsLayout = false;
-                    return;
-                }
-
-                var serverManager = ServerManager.Instance;
-                if (!serverManager || serverManager.CurrentRegion?.TryCast<DnsRegionInfo>() is not { } regionInfo || !regionInfo.Fqdn.EndsWith("among.us"))
-                {
-                    useDtlsLayout = false;
-                }
-            }
-        }
-
-        /// <summary>
-        /// Encrypt connection only to Among Us official servers
-        /// </summary>
-        [HarmonyPatch(typeof(AuthManager), nameof(AuthManager.CreateDtlsConnection))]
-        public static class DontEncryptCustomServersPatch
-        {
-            public static bool Prefix(ref UnityUdpClientConnection __result, string targetIp, ushort targetPort)
-            {
-                var serverManager = ServerManager.Instance;
-                if (serverManager.CurrentRegion.TryCast<DnsRegionInfo>() is not { } regionInfo || !regionInfo.Fqdn.EndsWith("among.us"))
-                {
-                    var remoteEndPoint = new IPEndPoint(IPAddress.Parse(targetIp), targetPort - 3);
-                    __result = new UnityUdpClientConnection(null, remoteEndPoint);
-                    return false;
-                }
-
-                return true;
-            }
+            __result = false;
+            return false;
         }
     }
 }
