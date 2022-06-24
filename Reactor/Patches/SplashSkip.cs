89f06e01f17b6eaf88519eb44881fb84fc265cb1
js6pak <kubastaron@hotmail.com>
2022-06-24 04:50:17 +0200

    Fix splash skip on epic

--- Reactor/Patches/SplashSkip.cs
+++ Reactor/Patches/SplashSkip.cs
@@ -1,4 +1,7 @@
+using System.Reflection;
 using HarmonyLib;
+using System;
+using MonoMod.Utils;
 
 namespace Reactor.Patches
 {
@@ -8,6 +11,25 @@ namespace Reactor.Patches
         private static void Prefix(SplashManager __instance)
         {
             __instance.minimumSecondsBeforeSceneChange = 0;
+        }
+    }
+
+    [HarmonyPatch(typeof(SplashManager), nameof(SplashManager.Update))]
+    internal static class WaitForEpicAuth
+    {
+        private static readonly PropertyInfo? _localUserIdProperty = typeof(EpicManager).GetProperty("localUserId", BindingFlags.Static | BindingFlags.Public);
+        private static readonly Func<object>? _localUserId = _localUserIdProperty == null ? null : _localUserIdProperty.GetMethod.CreateDelegate<Func<object>>();
+
+        private static bool Prefix(SplashManager __instance)
+        {
+            if (__instance.startedSceneLoad) return true;
+
+            if (_localUserId != null && _localUserId() == null)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
