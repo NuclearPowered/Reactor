b553741889862e37df531f9b6f9bec362d5fbedf
AlexejheroYTB <alexejhero@gmail.com>
2022-10-13 19:04:37 +0300

    Sort providers less and update currentlanguage when one is added

--- Reactor/Localization/LocalizationManager.cs
+++ Reactor/Localization/LocalizationManager.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Il2CppInterop.Runtime.InteropTypes.Arrays;
 
 namespace Reactor.Localization;
@@ -17,7 +16,12 @@ public static class LocalizationManager
     /// <param name="provider">A <see cref="LocalizationProvider"/> instance.</param>
     public static void Register(LocalizationProvider provider)
     {
-        if (!_providers.Contains(provider)) _providers.Add(provider);
+        if (!_providers.Contains(provider))
+        {
+            _providers.Add(provider);
+            provider.SetLanguage(TranslationController.Instance.currentLanguage.languageID);
+            _providers.Sort((a, b) => b.Priority - a.Priority);
+        }
     }
 
     /// <summary>
@@ -40,7 +44,7 @@ public static class LocalizationManager
 
     internal static bool TryGetTextFormatted(StringNames stringName, Il2CppReferenceArray<Il2CppSystem.Object> parts, out string text)
     {
-        foreach (var provider in _providers.OrderByDescending(p => p.Priority))
+        foreach (var provider in _providers)
         {
             if (provider.TryGetTextFormatted(stringName, parts, out text!))
             {
@@ -54,7 +58,7 @@ public static class LocalizationManager
 
     internal static bool TryGetText(StringNames stringName, out string text)
     {
-        foreach (var provider in _providers.OrderByDescending(p => p.Priority))
+        foreach (var provider in _providers)
         {
             if (provider.TryGetText(stringName, out text!))
             {
@@ -68,7 +72,7 @@ public static class LocalizationManager
 
     internal static bool TryGetStringName(SystemTypes systemType, out StringNames stringName)
     {
-        foreach (var provider in _providers.OrderByDescending(p => p.Priority))
+        foreach (var provider in _providers)
         {
             if (provider.TryGetStringName(systemType, out var stringNameNullable))
             {
@@ -83,7 +87,7 @@ public static class LocalizationManager
 
     internal static bool TryGetStringName(TaskTypes taskTypes, out StringNames stringName)
     {
-        foreach (var provider in _providers.OrderByDescending(p => p.Priority))
+        foreach (var provider in _providers)
         {
             if (provider.TryGetStringName(taskTypes, out var stringNameNullable))
             {
@@ -98,7 +102,7 @@ public static class LocalizationManager
 
     internal static void OnLanguageChanged(SupportedLangs newLanguage)
     {
-        foreach (var provider in _providers.OrderByDescending(p => p.Priority))
+        foreach (var provider in _providers)
         {
             provider.SetLanguage(newLanguage);
         }
