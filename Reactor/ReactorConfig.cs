32375415a9cafaaccae51cef2947098a56ca1ffb
js6pak <me@6pak.dev>
2023-12-04 21:57:21 +0100

    Add DisableServerAuthority to ModFlags

--- Reactor/ReactorConfig.cs
+++ Reactor/ReactorConfig.cs
@@ -6,7 +6,10 @@ internal static class ReactorConfig
 {
     public const string FeaturesSection = "Features";
 
+    public static ConfigEntry<bool> ForceDisableServerAuthority { get; private set; } = null!;
+
     public static void Bind(ConfigFile config)
     {
+        ForceDisableServerAuthority = config.Bind(FeaturesSection, nameof(ForceDisableServerAuthority), false, "Enables the DisableServerAuthority flag even when no mods declare it");
     }
 }
