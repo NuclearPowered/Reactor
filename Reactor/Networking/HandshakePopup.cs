6ee72ef41c7ce90507a76158def9c66ae3f8c2b6
js6pak <me@6pak.dev>
2024-03-02 12:54:02 +0100

    Update Reactor/Networking/HandshakePopup.cs
    Co-authored-by: miniduikboot <mini@duikbo.at>

--- Reactor/Networking/HandshakePopup.cs
+++ Reactor/Networking/HandshakePopup.cs
@@ -4,7 +4,7 @@ internal static class HandshakePopup
 {
     public const string Message =
         """
-        This server doesn't support modded handshake.
+        This server doesn't support Reactor's modded handshake.
         The lobbies shown may be incompatible with your current mods.
         For more info see <link=https://reactor.gg/handshake>reactor.gg/handshake</link>
         """;
