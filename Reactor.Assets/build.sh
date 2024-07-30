86b5a29b4eb67b9d32473a5685496d6050b8db91
js6pak <me@6pak.dev>
2024-07-31 00:07:12 +0200

    Update Unity version in Reactor.Assets
    <!-- ps-id: 7978e8c1-da06-45f4-9d7d-6f1591074889 -->

--- Reactor.Assets/build.sh
+++ Reactor.Assets/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-UNITY_VERSION=2020.3.22f1
+UNITY_VERSION=2020.3.45f1
 
 if [[ ! -v UNITY_PATH ]]; then
     if command -v unityhub &>/dev/null; then
         echo "Trying to get unity path from unityhub..."
 
-        UNITYHUB_INSTALL_PATH=$(unityhub --headless install-path)
+        UNITYHUB_INSTALL_PATH=$(unityhub -- --headless install-path)
         UNITY_PATH="$UNITYHUB_INSTALL_PATH/$UNITY_VERSION/Editor/Unity"
 
         if [[ ! -f $UNITY_PATH ]]; then
@@ -22,4 +22,4 @@ fi
 echo "UNITY_PATH = $UNITY_PATH"
 
 rm Library -rf # TODO figure out why having Library cached makes invalid bundles in batchmode
-$UNITY_PATH -batchmode -nographics -quit -logFile "-" -projectPath $pwd -executeMethod Build.BuildAssetBundles
+"$UNITY_PATH" -batchmode -nographics -quit -logFile "-" -projectPath "$PWD" -executeMethod Build.BuildAssetBundles
