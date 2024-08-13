524549fb298fa254e18c60b2c01a75945f144d8a
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2024-08-13 22:41:13 +0300

    Fix warnings (i forgor)

--- Reactor/Patches/ReactorPingTracker.cs
+++ Reactor/Patches/ReactorPingTracker.cs
@@ -33,7 +33,7 @@ public static class ReactorPingTracker
 #else
             IsDevBuild = false,
 #endif
-            ShouldShow = null//() => AmongUsClient.Instance.IsGameStarted,
+            ShouldShow = () => AmongUsClient.Instance.IsGameStarted,
         }
     ];
 
@@ -43,7 +43,7 @@ public static class ReactorPingTracker
     /// <param name="modName">The user-friendly name of the mod. Can contain spaces or special characters.</param>
     /// <param name="version">The version of the mod.</param>
     /// <param name="isDevOrBetaBuild">If this version is a development or beta version. If true, it will display the mod in red in the PingTracker.</param>
-    /// <param name="shouldShow">This function will be called every frame to determine if the mod should be displayed or not. This function should return false if your mod is currently disabled or has no effect on gameplay at the time. If you want the mod to be displayed at all times, set this parameter to null to avoid delegate calls.</param>
+    /// <param name="shouldShow">This function will be called every frame to determine if the mod should be displayed or not. This function should return false if your mod is currently disabled or has no effect on gameplay at the time. If you want the mod to be displayed at all times, you can set this parameter to null to avoid delegate calls.</param>
     public static void RegisterMod(string modName, string version, bool isDevOrBetaBuild, Func<bool>? shouldShow)
     {
         if (modName.Contains("</noparse>", StringComparison.OrdinalIgnoreCase) || version.Contains("</noparse>", StringComparison.OrdinalIgnoreCase))
