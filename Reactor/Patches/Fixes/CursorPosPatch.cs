bbdbd5bec87c32b0f6c793648def78637bd11d2b
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2024-08-13 21:13:03 +0300

    Fix ambiguous patch exception

--- Reactor/Patches/Fixes/CursorPosPatch.cs
+++ Reactor/Patches/Fixes/CursorPosPatch.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using HarmonyLib;
 using TMPro;
 using UnityEngine;
@@ -8,9 +10,14 @@ namespace Reactor.Patches.Fixes;
 /// <summary>
 /// "Fixes" an issue where empty TextBoxes have wrong cursor positions.
 /// </summary>
-[HarmonyPatch(typeof(TextMeshProExtensions), nameof(TextMeshProExtensions.CursorPos))]
+[HarmonyPatch]
 internal static class CursorPosPatch
 {
+    public static IEnumerable<MethodBase> TargetMethods()
+    {
+        return AccessTools.GetDeclaredMethods(typeof(TextMeshProExtensions)).Where(m => m.Name == nameof(TextMeshProExtensions.CursorPos));
+    }
+
     public static bool Prefix(TextMeshPro self, ref Vector2 __result)
     {
         if (self.textInfo == null || self.textInfo.lineCount == 0 || self.textInfo.lineInfo[0].characterCount <= 0)
