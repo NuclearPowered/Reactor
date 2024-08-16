c422098d96455c0be525b6af4c2ab18086ff266c
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2024-08-16 17:52:34 +0300

    Improve ModIdentifier struct and address reviews

--- Reactor/Patches/ReactorPingTracker.cs
+++ Reactor/Patches/ReactorPingTracker.cs
@@ -9,36 +9,21 @@ namespace Reactor.Patches;
 /// </summary>
 public static class ReactorPingTracker
 {
-    private struct ModIdentifier
+    private readonly struct ModIdentifier(string modName, string version, bool isDevBuild, Func<bool>? shouldShow)
     {
         private static string NormalColor => !AmongUsClient.Instance.IsGameStarted ? "#fff" : "#fff8";
         private static string DevColor => !AmongUsClient.Instance.IsGameStarted ? "#f00" : "#f008";
 
-        public string ModName;
-        public string Version;
-        public bool IsDevBuild;
-        public Func<bool>? ShouldShow;
+        public string ModName => modName;
+        public string Text => $"</noparse><color={(isDevBuild ? DevColor : NormalColor)}><noparse>{ModName} {version}</noparse></color><noparse>";
 
-        public readonly string Text => $"</noparse><color={(IsDevBuild ? DevColor : NormalColor)}><noparse>{ModName} {Version}</noparse></color><noparse>";
+        public bool ShouldShow() => shouldShow?.Invoke() ?? true;
     }
 
-    private static readonly List<ModIdentifier> _modIdentifiers =
-    [
-        new ModIdentifier
-        {
-            ModName = ReactorPlugin.Name,
-            Version = ReactorPlugin.Version,
-#if DEBUG
-            IsDevBuild = true,
-#else
-            IsDevBuild = false,
-#endif
-            ShouldShow = () => AmongUsClient.Instance.IsGameStarted,
-        }
-    ];
+    private static readonly List<ModIdentifier> _modIdentifiers = [];
 
     /// <summary>
-    /// Registers a mod with the PingTrackerManager, adding it to the list of mods that will be displayed in the PingTracker.
+    /// Registers a mod with the <see cref="ReactorPingTracker"/>, adding it to the list of mods that will be displayed in the PingTracker.
     /// </summary>
     /// <param name="modName">The user-friendly name of the mod. Can contain spaces or special characters.</param>
     /// <param name="version">The version of the mod.</param>
@@ -46,40 +31,40 @@ public static class ReactorPingTracker
     /// <param name="shouldShow">This function will be called every frame to determine if the mod should be displayed or not. This function should return false if your mod is currently disabled or has no effect on gameplay at the time. If you want the mod to be displayed at all times, you can set this parameter to null to avoid delegate calls.</param>
     public static void RegisterMod(string modName, string version, bool isDevOrBetaBuild, Func<bool>? shouldShow)
     {
+        if (modName.Length + version.Length > 60)
+        {
+            Error($"Not registering mod \"{modName}\" with version \"{version}\" in {nameof(ReactorPingTracker)} because the combined length of the mod name and version is greater than 60 characters.");
+            return;
+        }
+
         if (modName.Contains("</noparse>", StringComparison.OrdinalIgnoreCase) || version.Contains("</noparse>", StringComparison.OrdinalIgnoreCase))
         {
-            Error($"Not registering mod \"{modName}\" with version \"{version}\" in PingTrackerManager because it contains the string \"</noparse>\" which is disallowed.");
+            Error($"Not registering mod \"{modName}\" with version \"{version}\" in {nameof(ReactorPingTracker)} because it contains the string \"</noparse>\" which is disallowed.");
             return;
         }
 
         if (_modIdentifiers.Any(m => m.ModName == modName))
         {
-            Error($"Mod \"{modName}\" is already registered in PingTrackerManager.");
+            Error($"Mod \"{modName}\" is already registered in {nameof(ReactorPingTracker)}.");
             return;
         }
 
-        _modIdentifiers.Add(new ModIdentifier
-        {
-            ModName = modName,
-            Version = version,
-            IsDevBuild = isDevOrBetaBuild,
-            ShouldShow = shouldShow,
-        });
+        _modIdentifiers.Add(new ModIdentifier(modName, version, isDevOrBetaBuild, shouldShow));
 
         _modIdentifiers.Sort((a, b) => string.Compare(a.ModName, b.ModName, StringComparison.Ordinal));
 
         if (!isDevOrBetaBuild)
         {
-            Info($"Mod \"{modName}\" registered in PingTrackerManager with version {version}.");
+            Info($"Mod \"{modName}\" registered in {nameof(ReactorPingTracker)} with version {version}.");
         }
         else
         {
-            Warning($"Mod \"{modName}\" registered in PingTrackerManager with DEVELOPMENT/BETA version {version}.");
+            Warning($"Mod \"{modName}\" registered in {nameof(ReactorPingTracker)} with DEVELOPMENT/BETA version {version}.");
         }
     }
 
     internal static string GetPingTrackerText()
     {
-        return "<align=center><size=50%><noparse>                       " + string.Join(", ", _modIdentifiers.Where(m => m.ShouldShow?.Invoke() ?? true).Select(m => m.Text)) + "</noparse></size></align>";
+        return "<align=center><size=50%><noparse>                       " + string.Join(", ", _modIdentifiers.Where(m => m.ShouldShow()).Select(m => m.Text)) + "</noparse></size></align>";
     }
 }
