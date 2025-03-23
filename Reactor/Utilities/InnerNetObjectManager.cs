dd9796e65870f1e10dbcf1bb29dcb91a41ba5367
D1GQ <yourboylax@gmail.com>
2025-03-22 19:32:34 -0500

    clean up

--- Reactor/Utilities/InnerNetObjectManager.cs
+++ Reactor/Utilities/InnerNetObjectManager.cs
@@ -26,7 +26,7 @@ public static class InnerNetObjectManager
     }
 
     /// <summary>
-    /// Spawns a new instance of a <see cref="InnerNetObject"/> of type <typeparamref name="T"/>.
+    /// Spawns a new <see cref="InnerNetObject"/> locally and on the network of type <typeparamref name="T"/>.
     /// </summary>
     /// <typeparam name="T">The type of the <see cref="InnerNetObject"/> to spawn. Must inherit from <see cref="InnerNetObject"/>.</typeparam>
     /// <param name="ownerId">The owner ID for the spawned object. Defaults to -2, which typically means no specific owner.</param>
@@ -49,11 +49,10 @@ public static class InnerNetObjectManager
     /// <summary>
     /// Spawns an existing <see cref="InnerNetObject"/> instance on the network.
     /// </summary>
-    /// <typeparam name="T">The type of the <see cref="InnerNetObject"/> being spawned. Must inherit from <see cref="InnerNetObject"/>.</typeparam>
     /// <param name="netObj">The <see cref="InnerNetObject"/> instance to spawn.</param>
     /// <param name="ownerId">The owner ID for the spawned object. Defaults to -2, which typically means no specific owner.</param>
     /// <param name="spawnFlags">The spawn flags to use when spawning the object. Defaults to <see cref="SpawnFlags.None"/>.</param>
-    public static void SpawnNetObject<T>(this InnerNetObject netObj, int ownerId = -2, SpawnFlags spawnFlags = SpawnFlags.None) where T : InnerNetObject
+    public static void SpawnNetObject(this InnerNetObject netObj, int ownerId = -2, SpawnFlags spawnFlags = SpawnFlags.None)
     {
         AmongUsClient.Instance.Spawn(netObj, ownerId, spawnFlags);
     }
