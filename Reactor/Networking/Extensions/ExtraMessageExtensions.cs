5cb6c35dacc4f45b06fdfaaa26afc1ab868b90a5
AS <56699741+AlchlcDvl@users.noreply.github.com>
2025-06-05 11:43:39 -0400

    Enhancements and additions to Reactor Networking (#91)
    * Update ExtraMessageExtensions.cs
    
    Added enum methods
    
    * Update MessageSerializer.cs
    
    Add enum serialisation
    
    * Oops documentation fix
    
    * Fix errors + add long serialisation
    
    * Correction: ulong not long
    
    * Specify format
    
    * Add long serialisation + improve enum serialisation
    
    * Add a shorthand method
    
    * Removed generic enum write and fix long write
    
    * Add generic converter support + read enum improvement
    
    * Remove leftover code
    
    * Better long serialisation
    
    * Serialise the array directly
    
    * Allow serialising color values
    
    * Update .gitignore
    
    * Update ExtraMessageExtensions.cs
    
    * Discard changes to .gitignore

--- Reactor/Networking/Extensions/ExtraMessageExtensions.cs
+++ Reactor/Networking/Extensions/ExtraMessageExtensions.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Globalization;
 using Hazel;
 using Hazel.Udp;
+using Reactor.Networking.Serialization;
 using UnityEngine;
 
 namespace Reactor.Networking.Extensions;
@@ -12,10 +14,11 @@ public static class ExtraMessageExtensions
 {
     private const float MIN = -50f;
     private const float MAX = 50f;
+    private const float DIFF = MAX - MIN;
 
     private static float ReverseLerp(float t)
     {
-        return Mathf.Clamp((t - MIN) / (MAX - MIN), 0f, 1f);
+        return Mathf.Clamp01((t - MIN) / DIFF);
     }
 
     /// <summary>
@@ -33,6 +36,33 @@ public static class ExtraMessageExtensions
     }
 
     /// <summary>
+    /// Writes an Enum value to the <paramref name="writer"/>.
+    /// </summary>
+    /// <param name="writer">The <see cref="MessageWriter"/> to write to.</param>
+    /// <param name="value">The <see cref="Enum"/> to write.</param>
+    public static void Write(this MessageWriter writer, Enum value) => writer.Serialize(Convert.ChangeType(value, Enum.GetUnderlyingType(value.GetType()), CultureInfo.InvariantCulture));
+
+    /// <summary>
+    /// Writes a long value to the <paramref name="writer"/>.
+    /// </summary>
+    /// <param name="writer">The <see cref="MessageWriter"/> to write to.</param>
+    /// <param name="value">The <see cref="long"/> to write.</param>
+    public static void Write(this MessageWriter writer, long value) => writer.Write(BitConverter.GetBytes(value));
+
+    /// <summary>
+    /// Writes a color value to the <paramref name="writer"/>.
+    /// </summary>
+    /// <param name="writer">The <see cref="MessageWriter"/> to write to.</param>
+    /// <param name="value">The <see cref="Color32"/> to write.</param>
+    public static void Write(this MessageWriter writer, Color32 value)
+    {
+        writer.Write(value.r);
+        writer.Write(value.g);
+        writer.Write(value.b);
+        writer.Write(value.a);
+    }
+
+    /// <summary>
     /// Reads a <see cref="Vector2"/> from the <paramref name="reader"/>.
     /// </summary>
     /// <param name="reader">The <see cref="MessageReader"/> to read from.</param>
@@ -44,6 +74,43 @@ public static class ExtraMessageExtensions
 
         return new Vector2(Mathf.Lerp(MIN, MAX, x), Mathf.Lerp(MIN, MAX, y));
     }
+
+    /// <summary>
+    /// Reads an enum value and casts it to the specified type from a network message.
+    /// </summary>
+    /// <param name="reader">The <see cref="MessageReader"/> to read from.</param>
+    /// <typeparam name="T">The <see cref="Enum"/> type to convert to.</typeparam>
+    /// <returns>An <see cref="Enum"/> value from the <paramref name="reader"/>.</returns>
+    public static T ReadEnum<T>(this MessageReader reader) where T : struct, Enum => (T) reader.ReadEnum(typeof(T));
+
+    /// <summary>
+    /// Reads an enum value from a network message.
+    /// </summary>
+    /// <param name="reader">The <see cref="MessageReader"/> to read from.</param>
+    /// <param name="enumType">The type of the enum.</param>
+    /// <returns>The resulting enum value from the <paramref name="reader"/>.</returns>
+    public static object ReadEnum(this MessageReader reader, Type enumType) => Enum.ToObject(enumType, reader.Deserialize(Enum.GetUnderlyingType(enumType)));
+
+    /// <summary>
+    /// Reads a long value from a network message.
+    /// </summary>
+    /// <param name="reader">The <see cref="MessageReader"/> to read from.</param>
+    /// <returns>The resulting long value from the <paramref name="reader"/>.</returns>
+    public static long ReadInt64(this MessageReader reader) => BitConverter.ToInt64(reader.ReadBytes(8), 0);
+
+    /// <summary>
+    /// Reads a color value from a network message.
+    /// </summary>
+    /// <param name="reader">The <see cref="MessageReader"/> to read from.</param>
+    /// <returns>The resulting Color32 value from the <paramref name="reader"/>.</returns>
+    public static Color32 ReadColor32(this MessageReader reader) => new(reader.ReadByte(), reader.ReadByte(), reader.ReadByte(), reader.ReadByte());
+
+    /// <summary>
+    /// Reads a color value from a network message.
+    /// </summary>
+    /// <param name="reader">The <see cref="MessageReader"/> to read from.</param>
+    /// <returns>The resulting Color value from the <paramref name="reader"/>.</returns>
+    public static Color ReadColor(this MessageReader reader) => new(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle());
 
     /// <summary>
     /// Sends a message on the <paramref name="connection"/> with an <paramref name="ackCallback"/>.
