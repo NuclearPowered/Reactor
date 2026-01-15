a5956eb59ce5e36c579475ffdc7042ccd97cd27e
XtraCube <72575280+XtraCube@users.noreply.github.com>
2026-01-15 15:08:11 -0500

    Use compiler generated wrappers for patching

--- Reactor/Patches/Fixes/CoFindGamePatch.cs
+++ Reactor/Patches/Fixes/CoFindGamePatch.cs
@@ -1,13 +1,20 @@
+using System.Reflection;
 using HarmonyLib;
+using Reactor.Utilities;
 
 namespace Reactor.Patches.Fixes;
 
 /// <summary>
 /// Fixes Game Lists not working on servers using legacy matchmaking.
 /// </summary>
-[HarmonyPatch(typeof(AmongUsClient_CoFindGame), nameof(AmongUsClient_CoFindGame.MoveNext))]
+[HarmonyPatch]
 internal static class CoFindGamePatch
 {
+    public static MethodBase TargetMethod()
+    {
+        return StateMachineWrapper<AmongUsClient>.GetStateMachineMoveNext(nameof(AmongUsClient.CoFindGame))!;
+    }
+
     public static void Prefix()
     {
         if (AmongUsClient.Instance.LastDisconnectReason == DisconnectReasons.Unknown)
